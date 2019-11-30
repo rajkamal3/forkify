03db7402fa2363a757721d979c8a00f72cc1aff4
rajkamal3 <rkchenumalla@gmail.com>
2019-11-30 19:12:13 +0530

    new commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,9 @@
 // Global app controller
 import num from './test';
+import str from './models/Search';
+import {add, multiply, ID} from './views/searchView';
 
 console.log(`I imported ${num} from another module`);
+console.log(`Hi ${str}`);
+
+console.log(add(1, 2));
